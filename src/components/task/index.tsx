caf3fa1c97db8a2d38dbbfac8cd676ac5607b24a
iy06 <yuki.inoue@di-v.co.jp>
2021-04-04 15:06:52 +0900

    edit index.tsx

--- src/components/task/index.tsx
+++ src/components/task/index.tsx
@@ -5,13 +5,17 @@ import './style.css';
 
 interface Props {
   task: TaskType;
+  getMatchTask: (id: number) => void;
 }
 
 export const Task = (props: Props) => {
   return (
-    <div className='task' style={{ backgroundColor: new Date(props.task.deadlineDate) > new Date() ? 'white': 'rgb(250, 192, 194)'}}>
+    <div className='task'
+      style={{ backgroundColor: new Date(props.task.deadlineDate) > new Date() ? 'white': 'rgb(250, 192, 194)'}} 
+      onClick={ () => { props.getMatchTask(props.task.id) } }
+    >
       <span className='task_date'>{ props.task.deadlineDate }</span>
-      <div className='task_text_contents'>
+      <div className='task_text_contents' >
         <h3 className='task_title'>{ props.task.name }</h3>
         <p className='task_sentence'>{ props.task.explanation }</p>
       </div>
