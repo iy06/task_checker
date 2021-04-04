f0e7e921e2d67c0b68e20de4fba414b87cadc9ac
iy06 <yuki.inoue@di-v.co.jp>
2021-04-04 15:07:53 +0900

    edit index.tsx

--- src/components/toDoList/index.tsx
+++ src/components/toDoList/index.tsx
@@ -11,20 +11,32 @@ interface Props {
   title: string;
 }
 
-
 export const ToDoList = (props: Props) => {
   const [isOpen, setIsOpen] = useState(false);
   const [isListOpen, setIsListOpen] = useState(true);
+  const [selectTask, setSelectTask] = useState<TaskType | undefined>();
 
   const handleOpen = () => {
     setIsOpen(true);
   };
+
   const handleClose = () => {
+    setSelectTask(undefined);
     setIsOpen(false);
   };
+
   const handleOnClick = () => {
     setIsListOpen(!isListOpen);
   }
+
+  const getMatchTask = (id: number) => {
+    setSelectTask(
+      props.tasks.find((task: TaskType) => {
+        return id === task.id;
+      })
+    )
+    handleOpen();
+  };
 
   return (
     <div className='task_list'>
@@ -42,12 +54,13 @@ export const ToDoList = (props: Props) => {
           handleClose={ handleClose }
           isOpen={ isOpen }
           body='taskBody'
+          task={ selectTask }
         />
       </div>
 
       <div className='task_field'>
         { isListOpen && props.tasks.map((task: TaskType) => {
-          return <Task task={ task } key={ task.id }/>
+          return <Task task={ task } key={ task.id } getMatchTask={ getMatchTask }/>
         })}
       </div>
     </div>
